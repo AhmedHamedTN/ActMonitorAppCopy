825e6ab14dc01fcac0bda28b2b757f3754308df9
khalilcharfi <khalilcharfi@users.noreply.github.com>
2016-06-21 05:48:14 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/ahmed/actmonitorapp/MainActivity.java
+++ app/src/main/java/com/example/ahmed/actmonitorapp/MainActivity.java
@@ -3,18 +3,17 @@ package com.example.ahmed.actmonitorapp;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 public class MainActivity extends AppCompatActivity
@@ -39,9 +38,32 @@ public class MainActivity extends AppCompatActivity
             light = Light.getInstance(this);
             sensingButton = (Button) findViewById(R.id.sensingButton);
 
-            // Retrieve a PendingIntent that will perform a broadcast
-            Intent alarmIntent = new Intent(this, AlarmReceiver.class);
-            pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+
+
+
+
+
+
+
+
+
+
+          Calendar cur_cal = new GregorianCalendar();
+          cur_cal.setTimeInMillis(System.currentTimeMillis());//set the current time and date for this calendar
+
+          Calendar cal = new GregorianCalendar();
+          cal.add(Calendar.DAY_OF_YEAR, cur_cal.get(Calendar.DAY_OF_YEAR));
+          cal.set(Calendar.HOUR_OF_DAY, 18);
+          cal.set(Calendar.MINUTE, 32);
+          cal.set(Calendar.SECOND, cur_cal.get(Calendar.SECOND));
+          cal.set(Calendar.MILLISECOND, cur_cal.get(Calendar.MILLISECOND));
+          cal.set(Calendar.DATE, cur_cal.get(Calendar.DATE));
+          cal.set(Calendar.MONTH, cur_cal.get(Calendar.MONTH));
+          // Retrieve a PendingIntent that will perform a broadcast
+          Intent alarmIntent = new Intent(this, AlarmReceiver.class);
+          pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+          AlarmManager alarm = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+          alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), 30*1000, pendingIntent);
         }
 
         @Override
